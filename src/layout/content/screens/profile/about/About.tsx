241e43f389311ce68222bec9f41a55d37e90bd6d
Arenelin <belov.1098@mail.ru>
2023-12-26 16:38:32 +0700

    added a cell with brief information and styled the About block

--- src/layout/content/screens/profile/about/About.tsx
+++ src/layout/content/screens/profile/about/About.tsx
@@ -9,13 +9,17 @@ export const AboutUser = () => {
         <StyledAboutUser>
             <UserDescription>
                 <UserPhotoWrapper>
-                    <div></div>
+                    <PhotoUnderlay></PhotoUnderlay>
+                    <UserAvatar></UserAvatar>
                 </UserPhotoWrapper>
                 <UserBody>
                     <UserInfo>
                         <UserName>Nikita Akmaykin</UserName>
-                        <UserStatus>When I am building an empire, do not come to me without materials and
-                            tools.</UserStatus>
+                        <UserStatus>When I am building an empire, do not come to me without materials and tools.</UserStatus>
+                        <ShortInfoCell>
+                            <Icon iconId={'info'} w={'18'} h={'18'} viewBox={'0 0 17 17'}/>
+                            <LearnMore>Learn More</LearnMore>
+                        </ShortInfoCell>
                     </UserInfo>
                     <WrapperButtons>
                         <GeneralButton as={'a'} href={'#'} primary name={'Edit profile'}/>
@@ -31,27 +35,26 @@ export const AboutUser = () => {
 
 const StyledAboutUser = styled.div`
     width: 100%;
-    min-height: 322px;
+    min-height: 350px;
     margin-bottom: 20px;
-    background-image: url("https://sneg.top/uploads/posts/2023-03/1678037075_sneg-top-p-oboi-dlya-glavnogo-ekrana-priroda-instagra-49.jpg");
+    background-image: url("https://w.forfun.com/fetch/8b/8b69e755f97a0501ff93e1edee9bffd6.jpeg");
     background-repeat: no-repeat;
     background-position: 50% 50%;
     background-size: cover;
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
     border-radius: 25px;
+    position: relative;
 `
 
 const UserDescription = styled.div`
     width: 100%;
     background-color: #ffffff;
     display: flex;
-    justify-content: end;
+    justify-content: space-between;
     padding: 20px;
-    position: relative;
     border-radius: 10px;
     max-height: 139px;
+    position: absolute;
+    bottom: 0;
 `
 const UserBody = styled.div`
     display: flex;
@@ -59,11 +62,11 @@ const UserBody = styled.div`
     align-items: baseline;
     max-width: 700px;
     width: 100%;
+    margin-left: 40px;
 `
 
 const UserInfo = styled.div`
-    max-width: 450px;
-
+    max-width: 480px;
 `
 const UserName = styled.span`
     font-size: 21px;
@@ -74,23 +77,65 @@ const UserStatus = styled.p`
     font-size: 13px;
     line-height: 16px;
     font-family: -apple-system, system-ui, "Helvetica Neue", Roboto, sans-serif;
+    margin: 6px 0;
 `
 
 const UserPhotoWrapper = styled.div`
     width: 150px;
     height: 150px;
+    margin-top: -50px;
+    
+    position: relative;
+    
+`
+
+const PhotoUnderlay = styled.div`
+background-color: #ffffff;
+    width: 158px;
+    height: 158px;
+    position: absolute;
+    left: -4px;
+    bottom: 40px;
+    border-radius: 50%;
+`
+const UserAvatar = styled.div`
     background-image: url(${myPhoto});
     background-repeat: no-repeat;
     background-position: 50% 50%;
     background-size: cover;
     border-radius: 50%;
+    width: 150px;
+    height: 150px;
     position: absolute;
-    left: 20px;
-    bottom: 20%;
-
+    padding: 20px;
+    bottom: 44px;
+    left: 0;
 `
 
 const WrapperButtons = styled.div`
     display: flex;
-    gap: 10px;
+    gap: 6px;
+    margin-left: 15px;
+`
+
+const LearnMore = styled.span`
+color: #626d7a;
+    font-family: -apple-system, BlinkMacSystemFont, Roboto, "Open Sans", "Helvetica Neue", "Noto Sans Armenian", "Noto Sans Bengali", "Noto Sans Cherokee", "Noto Sans Devanagari", "Noto Sans Ethiopic", "Noto Sans Georgian", "Noto Sans Hebrew", "Noto Sans Kannada", "Noto Sans Khmer", "Noto Sans Lao", "Noto Sans Osmanya", "Noto Sans Tamil", "Noto Sans Telugu", "Noto Sans Thai", sans-serif;
+    font-size: 13px;
+    padding-top: 3px;
+    transition: all .4s;
+`
+
+const ShortInfoCell = styled.div`
+display: flex;
+    gap: 4px;
+    align-items: center;
+    padding: 3px 3px 2px;
+    max-width: 96px;
+    &:hover{
+        cursor: pointer;
+        ${LearnMore}{
+            text-decoration: underline;
+        }
+    }
 `
